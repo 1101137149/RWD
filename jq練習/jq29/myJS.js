2996f529aa5e3066471659b6df4ea5ebeaf5b36e
Your Name1101137149 <fgd8389@gmail.com>
2022-12-02 15:57:16 +0800

    event.preventDefault()

--- jq練習/jq29/myJS.js
+++ jq練習/jq29/myJS.js
@@ -2,10 +2,13 @@ $(function () {
 
 
     $("header h3").on("click", aaa)
-    function aaa() {
+    function aaa(event) {
 
 
-        $("nav").toggleClass("rrr")
+        $("nav").toggleClass("rrr");
+
+        //有超連結避免跳到最上面 建議要加下面這段!
+        event.preventDefault();
     }
 
 
